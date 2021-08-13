7bb0f7a5053e1f9c5c3432b6e64a804a8c06de2b
Simon Thiboutôt <simonthiboutot@hotmail.com>
2021-08-13 06:52:45 -0400

    Add ActiveModel HTML comment serializer.

--- lib/flaky_test_tracker.rb
+++ lib/flaky_test_tracker.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "active_model/serializers/html_comment"
 require_relative "flaky_test_tracker/version"
 require_relative "flaky_test_tracker/sources/abstract_source"
 require_relative "flaky_test_tracker/sources/github_source"
