f9c5e1f4ce8a9f4fc0e1535e58204171a00299ac
Simon Thiboutôt <simonthiboutot@hotmail.com>
2021-08-15 07:01:57 -0400

    Remove utils class HTML comment serializer.

--- lib/flaky_test_tracker.rb
+++ lib/flaky_test_tracker.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "flaky_test_tracker/version"
-require_relative "flaky_test_tracker/utils/html_comment_serializer"
 require_relative "flaky_test_tracker/utils/mixins/html_comment_serializer"
 require_relative "flaky_test_tracker/sources/abstract_source"
 require_relative "flaky_test_tracker/sources/github_source"
--- lib/flaky_test_tracker/utils/mixins/html_comment_serializer.rb
+++ lib/flaky_test_tracker/utils/mixins/html_comment_serializer.rb
@@ -7,16 +7,22 @@ module FlakyTestTracker
       module HTMLCommentSerializer
         # @param [String] value The value to serialize.
         # @return [String] The value to serialized as HTML comment.
-        # @see FlakyTestTracker::Utils::HTMLCommentSerializer.serialize
         def to_html_comment(value)
-          ::HTMLCommentSerializer.serialize(value)
+          document = Nokogiri::HTML::Document.new
+          # We need to base64 encode since comment:
+          # - can't start with: `>` or `->`
+          # - can't include: `--`
+          content = Base64.encode64(value)
+          comment = Nokogiri::XML::Comment.new(document, content)
+          comment.to_html
         end
 
         # @param [String] value HTML comment representing the value serialized.
         # @return [String] The value to deserialized.
-        # @see FlakyTestTracker::Utils::HTMLCommentSerializer.deserialize
         def from_html_comment(value)
-          ::HTMLCommentSerializer.deserialize(value)
+          document = Nokogiri::HTML(value)
+          comment = document.search(".//comment()").first
+          Base64.decode64(comment.content)
         end
       end
     end
