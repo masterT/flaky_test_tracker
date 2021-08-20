85f3dd3849bdddbf4abb5698fafaca3a8f810e9c
Simon Thiboutôt <simonthiboutot@hotmail.com>
2021-08-20 07:51:55 -0400

    Add factories.

--- lib/flaky_test_tracker.rb
+++ lib/flaky_test_tracker.rb
@@ -4,16 +4,64 @@ require_relative "flaky_test_tracker/version"
 require_relative "flaky_test_tracker/serializers/html_serializer"
 require_relative "flaky_test_tracker/serializers/test_html_serializer"
 require_relative "flaky_test_tracker/sources/github_source"
-require_relative "flaky_test_tracker/source_factory"
 require_relative "flaky_test_tracker/inputs/test_input"
 require_relative "flaky_test_tracker/models/test"
 require_relative "flaky_test_tracker/repositories/test/github_issue_repository"
 require_relative "flaky_test_tracker/rendering/erb_rendering"
 require_relative "flaky_test_tracker/reporter"
+require_relative "flaky_test_tracker/reporters/base_reporter"
+require_relative "flaky_test_tracker/reporters/stdout_reporter"
 require_relative "flaky_test_tracker/confinement"
 require_relative "flaky_test_tracker/deconfinement"
 
+# Flaky test tracker.
 module FlakyTestTracker
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.confinement(**arguments)
+    FlakyTestTracker::Confinement.new(
+      test_repository: test_repository(**arguments[:test_repository]),
+      context: arguments[:context],
+      source: source(**arguments[:source]),
+      reporter: reporter(
+        reporters: arguments[:reporters],
+        verbose: arguments[:verbose]
+      )
+    )
+  end
+
+  def self.deconfinement(**arguments)
+    FlakyTestTracker::Deconfinement.new(
+      test_repository: test_repository(**arguments[:test_repository]),
+      reporter: reporter(
+        reporters: arguments[:reporters],
+        verbose: arguments[:verbose]
+      )
+    )
+  end
+
+  def self.test_repository(type:, options:)
+    case type
+    when :github_issue
+      FlakyTestTracker::Repositories::Test::GitHubIssueRepository.build(**options)
+    else
+      raise ArgumentError, "Unkown test repository type #{type.inspect}"
+    end
+  end
+
+  def self.source(type:, options:)
+    case type
+    when :github
+      FlakyTestTracker::Sources::GitHubSource.build(**options)
+    else
+      raise ArgumentError, "Unkown source type #{type.inspect}"
+    end
+  end
+
+  def self.reporter(reporters: [], verbose: false)
+    reporters << FlakyTestTracker::Reporters::STDOUTReporter.new if verbose
+    FlakyTestTracker::Reporter.new(
+      reporters: reporters
+    )
+  end
 end
--- spec/flaky_test_tracker_spec.rb
+++ spec/flaky_test_tracker_spec.rb
@@ -4,4 +4,187 @@ RSpec.describe FlakyTestTracker do
   it "has a version number" do
     expect(FlakyTestTracker::VERSION).not_to be nil
   end
+
+  describe "::confinement" do
+    let(:arguments) do
+      {
+        test_repository: {
+          type: :github_issue,
+          options: {
+            client: { access_token: "0612bcf5b16a1ec368ef4ebb92d6be2f7040260b" },
+            repository: "foo/bar",
+            labels: ["flaky"],
+            title_template: "<%= test.reference %>",
+            body_template: "<%= test.reference %>"
+          }
+        },
+        context: {},
+        source: {
+          type: :github,
+          options: {
+            host: "mygithub.com",
+            repository: "foo/bar",
+            commit: "0612bcf5b16a1ec368ef4ebb92d6be2f7040260b",
+            branch: "main"
+          }
+        },
+        verbose: true,
+        reporters: [FlakyTestTracker::Reporters::BaseReporter.new]
+      }
+    end
+
+    it "returns an instance of FlakyTestTracker::Confinement" do
+      expect(described_class.confinement(**arguments)).to be_a(FlakyTestTracker::Confinement)
+    end
+  end
+
+  describe "::deconfinement" do
+    let(:arguments) do
+      {
+        test_repository: {
+          type: :github_issue,
+          options: {
+            client: { access_token: "0612bcf5b16a1ec368ef4ebb92d6be2f7040260b" },
+            repository: "foo/bar",
+            labels: ["flaky"],
+            title_template: "<%= test.reference %>",
+            body_template: "<%= test.reference %>"
+          }
+        },
+        verbose: true,
+        reporters: [FlakyTestTracker::Reporters::BaseReporter.new],
+        confinement_duration: 86_400 * 10
+      }
+    end
+
+    it "returns an instance of FlakyTestTracker::Deconfinement" do
+      expect(described_class.deconfinement(**arguments)).to be_a(FlakyTestTracker::Deconfinement)
+    end
+  end
+
+  describe "::test_repository" do
+    let(:type) { nil }
+    let(:options) { {} }
+    let(:arguments) do
+      {
+        type: type,
+        options: options
+      }
+    end
+
+    context "when type github_issue" do
+      let(:type) { :github_issue }
+      let(:options) do
+        {
+          client: { access_token: "0612bcf5b16a1ec368ef4ebb92d6be2f7040260b" },
+          repository: "foo/bar",
+          labels: ["flaky"],
+          title_template: "<%= test.reference %>",
+          body_template: "<%= test.reference %>"
+        }
+      end
+
+      it "returns an instance of FlakyTestTracker::Repositories::Test::GitHubIssueRepository" do
+        expect(described_class.test_repository(**arguments)).to be_a(
+          FlakyTestTracker::Repositories::Test::GitHubIssueRepository
+        )
+      end
+    end
+
+    context "when type is unkown" do
+      let(:type) { :unkown_type }
+
+      it "raise an ArgumentError" do
+        expect { described_class.test_repository(**arguments) }.to raise_error(ArgumentError, /unkown .* type/i)
+      end
+    end
+  end
+
+  describe "::source" do
+    let(:type) { nil }
+    let(:options) { {} }
+    let(:arguments) do
+      {
+        type: type,
+        options: options
+      }
+    end
+
+    context "when type github" do
+      let(:type) { :github }
+      let(:options) do
+        {
+          host: "mygithub.com",
+          repository: "foo/bar",
+          commit: "0612bcf5b16a1ec368ef4ebb92d6be2f7040260b",
+          branch: "main"
+        }
+      end
+
+      it "returns an instance of FlakyTestTracker::Sources::GitHubSource" do
+        expect(described_class.source(**arguments)).to be_a(FlakyTestTracker::Sources::GitHubSource)
+      end
+    end
+
+    context "when type is unkown" do
+      let(:type) { :unkown_type }
+
+      it "raise an ArgumentError" do
+        expect { described_class.source(**arguments) }.to raise_error(ArgumentError, /unkown .* type/i)
+      end
+    end
+  end
+
+  describe "::reporter" do
+    let(:reporter) { FlakyTestTracker::Reporters::BaseReporter.new }
+    let(:reporters) { [reporter] }
+    let(:verbose) { false }
+    let(:arguments) do
+      {
+        reporters: reporters,
+        verbose: verbose
+      }
+    end
+
+    context "when verbose true" do
+      let(:verbose) { true }
+
+      it "creates an reporter without reporters + FlakyTestTracker::Reporters::STDOUTReporter" do
+        expect(described_class.reporter(**arguments)).to be_a(FlakyTestTracker::Reporter).and(
+          have_attributes(
+            reporters: a_collection_containing_exactly(
+              reporter,
+              an_instance_of(FlakyTestTracker::Reporters::STDOUTReporter)
+            )
+          )
+        )
+      end
+    end
+
+    context "when verbose false" do
+      let(:verbose) { false }
+
+      it "creates an reporter with reporters" do
+        expect(described_class.reporter(**arguments)).to be_a(FlakyTestTracker::Reporter).and(
+          have_attributes(
+            reporters: a_collection_containing_exactly(
+              reporter
+            )
+          )
+        )
+      end
+    end
+
+    context "with only required attributes" do
+      let(:arguments) { {} }
+
+      it "creates an reporter without reporters" do
+        expect(described_class.reporter(**arguments)).to be_a(FlakyTestTracker::Reporter).and(
+          have_attributes(
+            reporters: []
+          )
+        )
+      end
+    end
+  end
 end
