85403972cb77a82bbde19f08cbd784fb45877a48
Simon Thiboutôt <simonthiboutot@hotmail.com>
2021-08-20 14:13:21 -0400

    Fix rendering.

--- lib/flaky_test_tracker/rendering/erb_rendering.rb
+++ lib/flaky_test_tracker/rendering/erb_rendering.rb
@@ -4,18 +4,16 @@ require "erb"
 
 module FlakyTestTracker
   module Rendering
-    # ERB templat rendering.
+    # ERB template rendering.
     class ERBRendering
       attr_reader :erb, :template
 
       def initialize(template:)
         @template = template
-        @erb = ERB.new(template)
       end
 
       def output(**locals)
-        context = OpenStruct.new(locals)
-        erb.result(context.instance_eval { binding })
+        ERB.new(template).result_with_hash(locals)
       end
     end
   end
--- spec/flaky_test_tracker/rendering/erb_rendering_spec.rb
+++ spec/flaky_test_tracker/rendering/erb_rendering_spec.rb
@@ -8,35 +8,10 @@ RSpec.describe FlakyTestTracker::Rendering::ERBRendering do
   let(:template) { "<%= foo %>" }
   let(:erb) { ERB.new(template) }
 
-  describe "::new" do
-    it "set attributes erb" do
-      expect(
-        described_class.new(
-          template: template
-        )
-      ).to have_attributes(
-        erb: an_instance_of(ERB).and(
-          have_attributes(
-            src: erb.src,
-            encoding: erb.encoding
-          )
-        )
-      )
-    end
-  end
-
   describe "#output" do
     let(:locals) { { foo: "bar" } }
 
-    it "calls result on erb with binding" do
-      allow(subject.erb).to receive(:result)
-
-      subject.output(**locals)
-
-      expect(subject.erb).to have_received(:result).with(
-        an_instance_of(Binding)
-      )
-    end
+    # TODO: Add better test.
 
     it "returns result" do
       expect(subject.output(**locals)).to eq "bar"
